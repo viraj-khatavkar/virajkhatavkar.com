fa5d389abb013029cc3061176dcb0d18484c19f9
Viraj Khatavkar <viraj.2438@gmail.com>
2020-06-18 18:23:45 +0530

    wip

---
+++ resources/views/google_analytics.blade.php
@@ -0,0 +1,12 @@
+@if(app()->environment() === 'production')
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169937196-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-169937196-1');
+    </script>
+
+@endif
